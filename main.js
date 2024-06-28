692c0e124934bf73a9da0522b249e95cf4008e52
Julirex7 <julirex7@gmail.com>
2024-06-28 09:34:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+  canvas = createCanvas(450, 400);
+  canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot(){    
+  save('Imagen.png');
+}
